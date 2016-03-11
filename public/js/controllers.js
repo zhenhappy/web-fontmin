7fe4531d2080f9fb05b0b4bf5f389ee345ddbb01
forsigner <chenzhiyi@chenzhiyideiMac.local>
2016-03-11 15:52:28 +0800

    add progressPercentage

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -9,7 +9,7 @@ export default angular.module('controllers', [])
     var vm = this;
 
     vm.status = 'INIT'; // 'INIT' | 'UPLOADING' | 'SUCCESS' | 'ERROR'
-    vm.text =`从明天起，
+    vm.text = `从明天起，
     做一个幸福的人,
     喂马、劈柴，周游世界。
     从明天起，
@@ -18,7 +18,7 @@ export default angular.module('controllers', [])
     面朝大海，
     `;
 
-    var str = `春暖花开。`;
+    var str =`春暖花开。`;
     var originArr = str.split('');
     var arr = [];
     originArr.forEach(elem => {
@@ -72,6 +72,7 @@ export default angular.module('controllers', [])
 
         vm.status = 'UPLOADING';
         var progressPercentage = parseInt(100.0 * evt.loaded / evt.total, 10);
+        vm.progressPercentage = progressPercentage + '%';
         console.log('progress: ' + progressPercentage + '% ' + evt.config.data.file.name);
       });
 
